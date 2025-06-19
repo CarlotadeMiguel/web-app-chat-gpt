2416c26358573a98da5726710940fa7cb90ca1d6
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-06-19 18:17:47 +0200

    feat(models): añadir modelos  SQLAlchemy de User y Chat

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from .extensions import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    chats = db.relationship('Chat', backref='user', lazy=True)
+
+class Chat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    role = db.Column(db.Enum('user', 'assistant'), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
